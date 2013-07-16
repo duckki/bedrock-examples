9b557b41086f0ad9682a95e4c34f0c1c9016e643
Duckki Oe <kain.nomad@gmail.com>
2013-07-16 09:14:50 -0400

    improved search-max proof

--- search-max.v
+++ search-max.v
@@ -32,6 +32,8 @@ Definition maxInv (x y : nat) ls :=
   let a_y := selN ls y in
   gtAll' a_x a_y (firstn x ls) /\ gtAll' a_x a_y (skipn y ls).
 
+Inductive loopBody := loopBodyI.
+
 Definition maxM := bmodule "max" {{
   bfunction "max"("a", "n", "x", "y", "a_x", "a_y", "offset") [maxS]
     "x" <- 0;;
@@ -43,7 +45,7 @@ Definition maxM := bmodule "max" {{
               * [| maxInv (wordToNat (V "x")) (wordToNat (V "y")) ls |]
       POST[R] array ls (V "a") * [| (wordToNat R < length ls)%nat |]
               * [| gtAll (selN ls (wordToNat R)) ls |] ]
-    While ("x" <> "y") {
+    While ("x" <> "y") { Note [loopBody];;
       "offset" <- 4 * "x";;
       "a_x" <-* "a" + "offset";;
       "offset" <- 4 * "y";;
@@ -83,11 +85,10 @@ Hint Resolve max_lem3.
 
 Lemma max_lem4 : forall n (x y : W), x <> y -> x <= y -> (wordToNat y < n)%nat
                                    -> (wordToNat (y ^- natToW 1) < n)%nat.
-  intros.
-  assert (x < y) by (apply wle_wneq_wlt; auto).
+  intros; assert (x < y) by (apply wle_wneq_wlt; auto).
   destruct_words; roundtrip; omega.
 Qed.
-
+Hint Resolve max_lem4.
 
 Lemma selN_last : forall ls : list W, selN ls (pred (Datatypes.length ls))
                                      = List.last ls $0.
@@ -103,16 +104,58 @@ Lemma skipn_last : forall ls : list W, ls <> nil
   rewrite IHls; auto; congruence.
 Qed.
 
+Lemma maxInv_init : forall n ls, ls <> nil -> wordToNat n = length ls
+                                 -> maxInv 0 (wordToNat (n ^- natToW 1)) ls.
+  unfold maxInv, gtAll'; split; simpl; auto.
+  assert (wordToNat (n ^- natToW 1) = pred (length ls)).
+  {
+    destruct ls; try congruence; simpl in *.
+    roundtrip; autorewrite with N; auto.
+    rewrite H0; omega.
+  }
+  rewrite H1.
+  rewrite selN_last, skipn_last by auto.
+  apply List.Forall_cons; auto.
+Qed.
+Hint Resolve maxInv_init.
+
 Lemma firstn_S : forall n ls, ls <> nil -> (S n < length ls)%nat
                               -> firstn (S n) ls
                                  = firstn n ls ++ selN ls n :: nil.
   induction n; try congruence; simpl; intros.
   destruct ls; try congruence; simpl; auto.
   destruct ls; try congruence; simpl; auto.
-  rewrite <- IHn; auto.
-  destruct ls; simpl in *; try congruence; try omega.
-  simpl in *; omega.
+  rewrite <- IHn; auto; simpl in *; try omega.
+  destruct ls; simpl in *; congruence || omega.
 Qed.
+
+Lemma maxInv_plus_h : forall x y ls,  maxInv x y ls -> selN ls x <= selN ls y
+                        -> (x < y)%nat -> (y < length ls)%nat
+                        -> maxInv (S x) y ls.
+  unfold maxInv; unfold gtAll'; intros.
+  assert (ls <> nil) by (destruct ls; simpl in *; congruence || omega).
+  destruct H; split.
+  {
+    rewrite firstn_S by auto.
+    apply Forall_app; auto.
+    eapply Forall_impl, H; simpl; intros.
+    destruct H5; right; nomega.
+  }
+  {
+    eapply Forall_impl, H4; simpl; intros.
+    destruct H5; right; nomega.
+  }
+Qed.
+
+Lemma maxInv_plus : forall (x y : W) ls, x <> y -> x <= y
+                        -> (wordToNat y < length ls)%nat
+                        -> selN ls (wordToNat x) <= selN ls (wordToNat y)
+                        -> maxInv (wordToNat x) (wordToNat y) ls
+                        -> maxInv (wordToNat (x ^+ natToW 1)) (wordToNat y) ls.
+  intros; destruct_words; roundtrip.
+  replace (w + 1) with (S w) by omega; apply maxInv_plus_h; auto; nomega.
+Qed.
+Hint Resolve maxInv_plus.
 
 Lemma skipn_pred : forall n ls, ls <> nil -> (n < length ls)%nat -> (0 < n)%nat
                               -> skipn (pred n) ls
@@ -121,8 +164,7 @@ Lemma skipn_pred : forall n ls, ls <> nil -> (n < length ls)%nat -> (0 < n)%nat
   destruct ls; try congruence; simpl; auto.
   destruct n; simpl; auto.
   rewrite <- IHn; simpl in *; auto.
-  destruct ls; simpl in *; try omega.
-  congruence.
+  destruct ls; simpl in *; omega || congruence.
 Qed.
 
 Lemma Forall_firstn_skipn : forall A P n (ls : list A),
@@ -132,73 +174,23 @@ Lemma Forall_firstn_skipn : forall A P n (ls : list A),
   intros; rewrite <- (firstn_skipn n); apply Forall_app; auto.
 Qed.
 
-
-Lemma maxInv_init : forall n ls, ls <> nil -> wordToNat n = length ls
-                                 -> maxInv 0 (wordToNat (n ^- natToW 1)) ls.
-  unfold maxInv, gtAll'; split; simpl; auto.
-  assert (wordToNat (n ^- natToW 1) = pred (length ls)).
-    destruct ls; try congruence; simpl in *.
-    roundtrip; autorewrite with N; auto.
-    rewrite H0; omega.
-  rewrite H1.
-  rewrite selN_last, skipn_last by auto.
-  apply List.Forall_cons; auto.
-  right; nomega.
-Qed.
-Hint Resolve maxInv_init.
-
-Lemma maxInv_plus' : forall x y ls, (x < y)%nat
-                               -> (y < length ls)%nat
-                               -> selN ls x <= selN ls y
-                               -> maxInv x y ls
-                               -> maxInv (S x) y ls.
-  unfold maxInv; unfold gtAll'; intros.
-  assert (ls <> nil).
-    destruct ls; try congruence; simpl in *.
-    exfalso; omega.
-  destruct H2; split.
-  rewrite firstn_S by auto.
-  apply Forall_app; auto.
-  eapply Forall_impl, H2; simpl; intros.
-  destruct H5; auto.
-  right; nomega.
-
-  eapply Forall_impl, H4; simpl; intros.
-  destruct H5; auto.
-  right; nomega.
-Qed.
-
-Lemma maxInv_plus : forall (x y : W) ls, x <> y -> x <= y
-                        -> (wordToNat y < length ls)%nat
-                        -> selN ls (wordToNat x) <= selN ls (wordToNat y)
-                        -> maxInv (wordToNat x) (wordToNat y) ls
-                        -> maxInv (wordToNat (x ^+ natToW 1)) (wordToNat y) ls.
-  intros; destruct_words; roundtrip.
-  replace (w + 1) with (S w) by omega.
-  apply maxInv_plus'; auto.
-  nomega.
-Qed.
-Hint Resolve maxInv_plus.
-
-Lemma maxInv_minus' : forall x y ls, (x < y)%nat -> (y < length ls)%nat
-                               -> selN ls y < selN ls x
-                               -> maxInv x y ls
+Lemma maxInv_minus_h : forall x y ls, maxInv x y ls -> selN ls y < selN ls x
+                               -> (x < y)%nat -> (y < length ls)%nat
                                -> maxInv x (pred y) ls.
   unfold maxInv; unfold gtAll'; intros.
-  assert (ls <> nil).
-    destruct ls; try congruence; simpl in *.
-    exfalso; omega.
-  destruct H2; split.
-  eapply Forall_impl, H2; simpl; intros.
-  destruct H5; auto.
-  left; nomega.
-
-  rewrite skipn_pred by auto.
-  eapply Forall_cons.
-  right; nomega.
-  eapply Forall_impl, H4; simpl; intros.
-  destruct H5; auto.
-  left; nomega.
+  assert (ls <> nil) by (destruct ls; simpl in *; congruence || omega).
+  destruct H; split.
+  {
+    eapply Forall_impl, H; simpl; intros.
+    destruct H5; auto; left; nomega.
+  }
+  {
+    rewrite skipn_pred by auto.
+    eapply Forall_cons.
+    right; nomega.
+    eapply Forall_impl, H4; simpl; intros.
+    destruct H5; auto; left; nomega.
+  }
 Qed.
 
 Lemma maxInv_minus : forall (x y : W) ls, x <> y -> x <= y
@@ -208,8 +200,7 @@ Lemma maxInv_minus : forall (x y : W) ls, x <> y -> x <= y
                          -> maxInv (wordToNat x) (wordToNat (y ^- natToW 1)) ls.
   intros; destruct_words; roundtrip.
   replace (w0 - 1) with (pred w0) by omega.
-  apply maxInv_minus'; auto.
-  nomega.
+  apply maxInv_minus_h; auto; nomega.
 Qed.
 Hint Resolve maxInv_minus.
 
@@ -217,8 +208,7 @@ Lemma maxInv_gtAll : forall x ls, maxInv x x ls -> gtAll (selN ls x) ls.
   unfold maxInv, gtAll, gtAll'; intros; subst.
   destruct H.
   eapply Forall_impl, Forall_firstn_skipn; try eassumption.
-  simpl; intros.
-  destruct H1; auto.
+  simpl; destruct 1; auto.
 Qed.
 Hint Resolve maxInv_gtAll.
 
@@ -227,6 +217,7 @@ Lemma maxInv_eq : forall (x y : W) ls, x = y
                       -> maxInv (wordToNat x) (wordToNat x) ls.
   intros; subst; auto.
 Qed.
+Hint Resolve maxInv_eq.
 
 
 (* ============================================================================
@@ -234,12 +225,11 @@ Qed.
  * ========================================================================= *)
     
 Hint Extern 1 (_ <= _) => nomega.
-Hint Extern 1 (wordToNat (_ ^- _) < _)%nat => solve [eapply max_lem4; eauto].
-Hint Extern 1 (maxInv _ _ _) => solve [eapply maxInv_eq; eauto].
 
 Ltac unstuck :=
   match goal with
-    | H: context[Assign] |- _ => generalize dependent H; evaluate auto_ext
+    | _: loopBody, H: context[Assign] |- _
+      => generalize dependent H; evaluate auto_ext
   end; intro;
   match goal with
     | _: ?x <= ?y, _: (wordToNat ?y < length ?ls)%nat |- _
@@ -247,12 +237,6 @@ Ltac unstuck :=
         assert (y < natToW (length ls)) by nomega
   end.
 
-Ltac finish :=
-  match goal with
-    | _ => solve [auto]
-    | H: Regs ?st Rv = _ |- _ => rewrite H
-  end.
-
 Theorem maxM_correct : moduleOk maxM.
-  vcgen; post; try unstuck; sep_auto; repeat finish.
+  vcgen; try enterFunction; post; try unstuck; sep_auto; eauto 3.
 Qed.
